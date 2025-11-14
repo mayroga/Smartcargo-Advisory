c0159c75e6ae41351203e77238a24f97020ef6ed
mayroga <132472344+mayroga@users.noreply.github.com>
2025-11-14 00:57:47 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
-// client/src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ShipmentForm from './components/ShipmentForm.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="min-h-screen flex items-center justify-center p-6">
-      <ShipmentForm />
-    </div>
+    <ShipmentForm />
   </React.StrictMode>
 );
