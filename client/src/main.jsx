18cf17d10d221ced5c0aa3af9a993710c4db5e61
mayroga <132472344+mayroga@users.noreply.github.com>
2025-11-14 00:39:00 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
+// client/src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ShipmentForm from './components/ShipmentForm.jsx'; // Importa tu formulario
+import ShipmentForm from './components/ShipmentForm.jsx';
 
-// Este archivo monta el componente principal en el <div> con id="root" del index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ShipmentForm /> 
-  </React.StrictMode>,
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <ShipmentForm />
+    </div>
+  </React.StrictMode>
 );
